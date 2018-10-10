4931af9a519eb44fc6b8e260d9a82006e0a2c1bf
Jay Shah <Jay9596@users.noreply.github.com>
2018-10-11 00:51:31 +0530

    Add website model

---
+++ model/website.js
@@ -0,0 +1,14 @@
+const sequelize = require("./index");const Sequelize = require("sequelize");
+const User = require("./user");
+const Website = sequelize.define("websites", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  url: Sequelize.STRING,
+  status: Sequelize.STRING
+});
+
+Website.belongsTo(User);
+
+module.exports = Website;
